2956990927bd775dc7afb4432706d48a2b2cb20b
jeikerxiao <jeiker@126.com>
2019-05-24 16:30:37 +0800

    add: SofaBoot Demo

---
+++ sofa-boot-demo1/src/main/java/com/jeiker/demo1/SofaBootDemo1Application.java
@@ -0,0 +1,13 @@
+package com.jeiker.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofaBootDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofaBootDemo1Application.class, args);
+    }
+
+}
